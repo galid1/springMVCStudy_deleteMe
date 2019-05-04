95da941de7489684c98f085bb813cfda6250d40e
jjy <galid1@naver.com>
2019-05-04 21:19:38 +0900

    @ComponentScan , AnnotationConfigApplicationContext를 이용해서 IoC Container사용.

--- src/main/java/com.galid/di/MainClass.java
+++ src/main/java/com.galid/di/MainClass.java
@@ -1,11 +1,12 @@
 package com.galid.di;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainClass {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:di.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         Student student = context.getBean("student", Student.class);
         System.out.println(student.getName());
     }
