c7cc17491457e7f77df5bc632bbdc4f4019930e0
jjy <galid1@naver.com>
2019-05-04 21:41:04 +0900

    ContextParam, listener을 이용해 ServletContext에 ApplicationContext추가하고, servlet에서 IoC Container 사용하기

---
+++ src/main/java/com.galid/mvc/service/NameService.java
@@ -0,0 +1,10 @@
+package com.galid.mvc.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NameService {
+    public String getName(){
+        return "jjy";
+    }
+}
--- src/main/java/com.galid/servlet/HelloServlet.java
+++ src/main/java/com.galid/servlet/HelloServlet.java
@@ -1,8 +1,11 @@
 package com.galid.servlet;
 
 
+import com.galid.mvc.service.NameService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+
 import javax.servlet.ServletException;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,8 +15,11 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ApplicationContext context = (ApplicationContext) getServletContext().getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
+        NameService nameService = context.getBean("nameService", NameService.class);
+
         resp.getWriter().println("<html><body>");
-        resp.getWriter().println("hello");
+        resp.getWriter().println("hello, " + nameService.getName());
         resp.getWriter().println("</body></html>");
     }
 
