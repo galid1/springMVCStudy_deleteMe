fd59b3abb3c216663550596200142bf1e1d0d019
jjy <galid1@naver.com>
2019-05-04 21:03:01 +0900

    classpath 추가, ClassPathXmlApplicationContext를 이용한 Bean사용

---
+++ src/main/java/com.galid/di/MainClass.java
@@ -0,0 +1,13 @@
+package com.galid.di;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainClass {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:di.xml");
+        Student student = context.getBean("student", Student.class);
+
+        System.out.println(student.getName());
+    }
+}
---
+++ src/main/java/com.galid/di/Student.java
@@ -0,0 +1,13 @@
+package com.galid.di;
+
+public class Student {
+    private String name = "jjy";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
